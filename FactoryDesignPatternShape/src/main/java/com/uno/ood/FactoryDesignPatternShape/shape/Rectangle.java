db7a7efa0404e336cf55410844b7177be31117e2
jmoseman01 <jmoseman01@gmail.com>
2017-08-08 09:02:06 -0500

    These shapes must have not got committed.

---
+++ FactoryDesignPatternShape/src/main/java/com/uno/ood/FactoryDesignPatternShape/shape/Rectangle.java
@@ -0,0 +1,9 @@
+package com.uno.ood.FactoryDesignPatternShape.shape;
+
+public class Rectangle implements Shape {
+
+	public void draw() {
+		System.out.println("ran Rectangle::draw()");
+	}
+
+}
---
+++ FactoryDesignPatternShape/src/main/java/com/uno/ood/FactoryDesignPatternShape/shape/Square.java
@@ -0,0 +1,9 @@
+package com.uno.ood.FactoryDesignPatternShape.shape;
+
+public class Square implements Shape {
+
+	public void draw() {
+		System.out.println("ran Square::draw()");
+	}
+
+}
