fac93ae5286825c8cbeb6181bf7205bf15d8f935
jemoseman <jemoseman@spl-22.lib.unomaha.edu>
2017-08-12 12:32:27 -0500

    BuilderPatternDemo Start

---
+++ BuilderPatternDemo/src/Meal.java
@@ -0,0 +1,6 @@
+import java.util.ArrayList;
+
+public class Meal {
+	public ArrayList<Item> meal = new ArrayList<Item>();
+
+}
---
+++ BuilderPatternDemo/src/MealBuilder.java
@@ -0,0 +1,12 @@
+
+public class MealBuilder {
+	public Meal prepareVegMeal() 
+	{
+		
+	}
+	public Meal prepareNonVegMeal() 
+	{
+		
+	}
+
+}
