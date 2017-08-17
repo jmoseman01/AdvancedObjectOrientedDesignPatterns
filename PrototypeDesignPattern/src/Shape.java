8fb76a5418b4067244a0752eddef5a835da38f67
unouser <unouser@pkipub-pc01.unomaha.edu>
2017-08-17 16:49:19 -0500

    Shape abstract class

---
+++ PrototypeDesignPattern/src/Shape.java
@@ -0,0 +1,36 @@
+
+public abstract class Shape {
+	private String id;
+	public String type;
+	
+	public String getType() 
+	{
+		return type;
+	}
+	public String getId() 
+	{
+		return id;
+	}
+	public void setId(String id) 
+	{
+		this.id=id;
+	}
+	public Object clone() 
+	{
+		Object clone=null;
+		
+		//run object clone method
+		//if object can't be cloned then throw 
+		//   the exception with the stacktrace
+		try 
+		{
+			clone=super.clone();
+		}
+		catch(CloneNotSupportedException e) 
+		{
+			e.printStackTrace();
+		}
+		return clone;
+	}
+
+}
