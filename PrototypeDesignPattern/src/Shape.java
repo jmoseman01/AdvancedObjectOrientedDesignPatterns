a0da13e4f70a0d5392ca584ac0cb58e7eb2d2f6e
unouser <unouser@pkipub-pc01.unomaha.edu>
2017-08-17 16:55:13 -0500

    add abstract method draw to shape and the Rectangle shape.

--- PrototypeDesignPattern/src/Shape.java
+++ PrototypeDesignPattern/src/Shape.java
@@ -2,32 +2,30 @@
 public abstract class Shape {
 	private String id;
 	public String type;
-	
-	public String getType() 
-	{
+
+	abstract void draw();
+
+	public String getType() {
 		return type;
 	}
-	public String getId() 
-	{
+
+	public String getId() {
 		return id;
 	}
-	public void setId(String id) 
-	{
-		this.id=id;
+
+	public void setId(String id) {
+		this.id = id;
 	}
-	public Object clone() 
-	{
-		Object clone=null;
-		
-		//run object clone method
-		//if object can't be cloned then throw 
-		//   the exception with the stacktrace
-		try 
-		{
-			clone=super.clone();
-		}
-		catch(CloneNotSupportedException e) 
-		{
+
+	public Object clone() {
+		Object clone = null;
+
+		// run object clone method
+		// if object can't be cloned then throw
+		// the exception with the stacktrace
+		try {
+			clone = super.clone();
+		} catch (CloneNotSupportedException e) {
 			e.printStackTrace();
 		}
 		return clone;
