016ffbbe1080ead0f55dc2997246e4bb43e100d8
unouser <unouser@pkipub-pc01.unomaha.edu>
2017-08-30 19:51:41 -0500

    Criteria start of implementation

---
+++ CriteriaPatternDemo/src/CriteriaFemale.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CriteriaFemale implements Criteria {
+
+	@Override
+	public List<Person> meetCriteria(List<Person> persons) {
+		// TODO Auto-generated method stub
+
+		List<Person> femalePersons=new ArrayList<Person>();
+		for(Person person: persons)
+		{
+			if(person.getGender().equalsIgnoreCase("female"))
+			{
+				femalePersons.add(person);
+			}
+		}
+		return femalePersons;
+	}
+
+}
