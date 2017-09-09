9ccde086b77cde66645ad53b0a8ff44d692304dc
unouser <unouser@pkipub-pc12.unomaha.edu>
2017-09-09 15:54:52 -0500

    initial facade imp

---
+++ FacadePatternDemo/src/ShapeMaker.java
@@ -0,0 +1,15 @@
+
+public class ShapeMaker {
+
+	private Circle circle;
+	private Square square;
+	private Rectangle rectangle;
+
+	public ShapeMaker() {
+		// TODO Auto-generated constructor stub
+		circle = new Circle();
+		square = new Square();
+		rectangle = new Rectangle();
+	}
+
+}
