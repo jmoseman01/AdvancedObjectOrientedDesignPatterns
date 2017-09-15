c31caf1bafff4efae324a20b9d311699f49de846
unouser <unouser@pkipub-pc01.unomaha.edu>
2017-09-15 11:11:42 -0500

    mistake on constructing loggers and their integer values

--- ChainOfResponsibility/src/Main.java
+++ ChainOfResponsibility/src/Main.java
@@ -18,8 +18,8 @@ public class Main {
 
 	private static AbstractLogger getChainOfLoggers() {
 		// TODO Auto-generated method stub
-		AbstractLogger errorLogger = new ErrorLogger(AbstractLogger.DEBUG);
-		AbstractLogger fileLogger = new FileLogger(AbstractLogger.INFO);
+		AbstractLogger errorLogger = new ErrorLogger(AbstractLogger.ERROR);
+		AbstractLogger fileLogger = new FileLogger(AbstractLogger.DEBUG);
 		AbstractLogger consoleLogger = new ConsoleLogger(AbstractLogger.INFO);
 		
 		errorLogger.setNextLogger(fileLogger);
